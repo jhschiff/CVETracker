59cb34dd3de534a902a5948c4cce09e4037da3fd
jhschiff <jordan41schiff@gmail.com>
2025-06-26 23:15:02 -0400

    updated server

--- server.js
+++ server.js
@@ -15,5 +15,7 @@ app.get('/kev', async (req, res) => {
   }
 });
 
-const PORT = 4000;
-app.listen(PORT, () => console.log(`Proxy server running on port ${PORT}`)); 
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Proxy server running on port ${PORT}`);
+}); 
